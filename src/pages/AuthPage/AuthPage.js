339c7a7dcf422ece671d9f49451c5f687e4999e3
Devon Aimes <aimes.devon@gmail.com>
2023-01-19 08:09:54 -0500

    Auth Page cleaned and finished

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,15 +1,4 @@
-// import SignUpForm from "../../components/SignUpForm/SignUpForm"
-// import LoginForm from "../../components/LoginForm/LoginForm"
 
-// export default function AuthPage(props){
-//     return(
-//       <main>
-//         <h1>AuthPage</h1>
-//         <SignUpForm setUser={props.setUser}/>
-//         <LoginForm setUser={props.setUser}/>
-//       </main>
-//     )
-// }
 import { useState } from 'react';
 import styles from './AuthPage.module.scss';
 import LoginForm from '../../components/LoginForm/LoginForm';
